daf8fee2ff24f3b63ecc7aa19b49c90d50a0c96e
Pedro Vinicius Wrublak <wrublakpedro@gmail.com>
2026-03-02 19:35:08 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <main>
+      <h1>Dynamox Landing</h1>
+    </main>
+  )
+}
+
+export default App
+// ```
+
+// ---
+
+// ### 🗂️ Create your folder structure
+
+// Inside `src/`, create these folders:
+// ```
+// src/
+// ├── components/
+// │   ├── Header/
+// │   ├── Hero/
+// │   ├── About/
+// │   ├── FeatureCards/
+// │   └── Footer/
+// ├── styles/
+// ├── types/
+// └── App.tsx
